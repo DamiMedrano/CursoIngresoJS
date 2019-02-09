659b7d18d15ba1ce84fef500c21edf1fb41b466a
DamiMedrano <38839937+DamiMedrano@users.noreply.github.com>
2019-02-09 11:47:24 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,21 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	alert("Esto funciona de maravilla");
+
+	//console.log("HOLA");
+
+	//document.write("<h1>hola</h1>");
 }
+
+/* Para salida de datos:
+
+alert("")
+document.write("")
+document.getElementById("")
+
+Entradas de datos:
+
+prompt("")
+document.get.ElementById("")
 
