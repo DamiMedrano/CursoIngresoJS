040f111e7292ee583c792b6b3610e615d61b935c
Dami Medrano <38839937+DamiMedrano@users.noreply.github.com>
2019-02-15 22:12:59 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -3,27 +3,3 @@ function mostrar()
 {
 	alert("Esto funciona de maravilla");
 }
-
-
-
-
-/*NOTAS
-----------------------------------------------------------------
-Salida interna:
-console.log("HOLA");
-
-Puede usarse con codigo HTML:
-document.write("<h1>hola</h1>");
-
->Salida de datos:
-alert("")
-document.write("")
-document.getElementById("")
-
-
->Entradas de datos:
-prompt("")
-document.get.ElementById("")
-
------------------------------------------------------------------
-*/
