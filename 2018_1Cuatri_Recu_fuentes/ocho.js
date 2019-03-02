a3aefa78249ed58cf662b9cbb41faf50f4de86ab
Dami Medrano <38839937+DamiMedrano@users.noreply.github.com>
2019-03-01 22:16:01 -0300

    Add files via upload

--- 2018_1Cuatri_Recu_fuentes/ocho.js
+++ 2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,4 +1,72 @@
+/*
+Realizar el algoritmo que permita iterar el ingreso de dos datos, un país y la
+superficie de su territorio (validar) hasta que el usuario quiera e informar al terminar
+el ingreso por document.write:
+1-La cantidad de países con superficie impar.
+2-La cantidad de países con superficie menor a 100
+3-La cantidad de países con superficie igual a 100
+4-El nombre del primer país que superó los 100 de superficie
+5-El promedio de kilómetros ingresados.
+6-El nombre del que menos territorio tiene.
+*/
+
 function mostrar()
 {
+	var pais;
+	var paisMenorSup;
+	var supPaisMenorSup;
+	var contSupImpar=0;
+	var contSupMenor100=0;
+	var contSupMayor100=0;
+	var primeraVezMayor;
+	var primeraVezMenor;
+	var primerPaisSupMayor100;
+	var superficie;
+	var promedioSup;
+	var sumaSup=0;
+
+	var respuesta;
+
+	do{
+		
+		pais=prompt("Ingrese el nombre del País");
+
+		do{
+			superficie=prompt("Ingrese el nombre del País");
+		}while(superficie.isNaN());
+
+		superficie=parseInt(superficie);
+
+		sumaSup+=superficie;
+
+		if(superficie%2==1){
+			contSupImpar++;
+		}
+		else{
+			if(superficie>100){
+				contSupMayor100++;
+				if(primeraVezMayor){
+					primerPaisSupMayor100=pais;
+					primeraVezMayor=false;
+				}
+			}
+			else{
+				contSupMenor100++;
+				if(primeraVezMenor){
+					paisMenorSup=pais;
+					supPaisMenorSup=superficie;
+					primeraVezMenor=false;
+				}
+				if(supPaisMenorSup>superficie){
+					supPaisMenorSup=superficie;
+					paisMenorSup=pais;
+				}
+			}
+		}
+
+		respuesta = confirm("seguir?");
+	}while(respuesta);
+
+	document.write( " país/es con superficie impar ")
 
 }
--- 2018_1Cuatri_Recu_fuentes/uno.js
+++ 2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,66 @@
+/*Realizar el algoritmo que permita el ingreso por prompt de las velocidades en
+kilómetros (validar entre 0 y 250) , el tipo de combustible, validar el tipo “s” o “l”
+para solido o liquido, de 5 vehículos de prueba ,informar por alert:
+1-El promedio de las velocidades totales.
+2-La velocidad más baja y el tipo de combustible de ese vehículo.
+3-La cantidad de combustibles líquidos que su velocidad supere los 100
+kilómetros.
+4-la velocidad más alta de los de combustible sólidos*/
 
 function mostrar()
 {
+	var velocidad;
+	var sumaVelocidades=0;
+	var promedioVelovidades;
+	var velocidadMasBaja=250;
+	var velocidadMaximaSolidos=0;
+	var combustibleVelocidadMinima; 
+	var combustible;
+	var contLiquidosSobre100=0;
+	var contadorAutos=0;
+
+	while(contadorAutos<5){
+		contadorAutos++;
+
+		//VELOCIDAD:
+		do{
+			velocidad=prompt("Ingrese la velocidad entre 0 y 250");
+			velocidad=parseInt(velocidad);
+		}while(velocidad<0 || velocidad>250 && isNaN(velocidad));
+
+		sumaVelocidades=sumaVelocidades+velocidad;
+		
+		//cOMBUSTIBLE:
+		do{
+			combustible=prompt("Ingrese “s” o “l” para solido o liquido");
+		} while(combustible!="s" && combustible!="l");
+		
+		//PUNTO 2
+		if(velocidadMasBaja>velocidad){
+			velocidadMasBaja=velocidad;
+			combustibleVelocidadMinima=combustible;
+		}
+		//PUNTO 3
+		if(combustible=="l" && velocidad>100){
+			contLiquidosSobre100++;
+		}
+		//PUNTO 4
+		if(combustible=="s"){
+			if(velocidadMaximaSolidos<velocidad){
+				velocidadMaximaSolidos=velocidad;
+			}
+		}
+		
+	}
+
+	promedioVelovidades=sumaVelocidades/5;
+
+	alert("El promedio de las velocidades es: " + promedioVelovidades);
+
+	alert("La cantidad de vehiculos de combustible liquido que superaron a 100km/h: " + contLiquidosSobre100);
+
+	alert("Velocidad más baja fue: " + velocidadMasBaja + " y el combustible fue: " + combustibleVelocidadMinima);
+
+	alert("La velocidad mas alta de los solidos fue: " + velocidadMaximaSolidos);
 
 }
